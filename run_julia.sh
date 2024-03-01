7db7e5ffa0734c4ae85b674f1277a42dda5b3017
Matt Woodstock <58792057+fishesofthedeep@users.noreply.github.com>
2024-03-01 15:54:22 -0500

    Update run_julia.sh

--- run_julia.sh
+++ run_julia.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-user=matthew.woodstock@whoi.edu  # Where to send mail
 #SBATCH --ntasks=1                  # Run on a single CPU
 #SBATCH --cpus-per-task=4	# Nubmer of CPU cores per task
-#SBATCH --mem=5gb                   # Job memory request
+#SBATCH --mem=10gb                   # Job memory request
 #SBATCH --time=24:00:00             # Time limit hrs:min:sec
 #SBATCH --output=serial_job_%j.err  # Standard error
 #SBATCH --output=serial_job_%j.out  # Standard output
